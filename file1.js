02720b4189bab312e98506564895fd5d10494296
vipul7070 <vipulkumar96ara@gmail.com>
2023-03-26 11:16:00 +0530

    commit

---
+++ file1.js
@@ -0,0 +1,163 @@
+// function add(num1,num2){
+//     console.log(num1+num2);
+// }
+// add(2,5);
+
+
+// const vipul=(a ,b)=>{
+//     console.log(a+b);
+
+// }
+// vipul(2, 5);
+// console.log('hello'); 
+
+
+//  //let age = prompt('enter your age?');
+//     // if(age !=null){
+//      //document.write(`your age is ${age}`);
+//      //}
+//     // else{
+//     //    document.write(`your age is not define`);
+//     // }
+// /////////////////////////////////////////////////////////////////////
+//   //let agree = confirm('are you sure?');
+//   //if(agree){
+//    // document.write("deleted")
+//   //}
+//   //else{
+//    // document.write("not deleted")
+//   //}
+
+//   //let type =6;
+//   //console.log(typeof type);
+//    //let newtype =String(type);
+//    //console.log( typeof newtype);
+
+   
+
+// function add (a , b)
+// {
+//  return a+b;
+
+// }  
+//   let c = add(4,8);
+//   console.log(c);
+
+//    console.log("anonymose function");
+
+
+
+
+// setTimeout( function (){ console.log("yyyyyyyyyyyy");
+// }  ,3000);
+
+
+//   console.log("immediately invoked functin"); 
+
+// (function (){
+//     console.log("kya karoge jaan kar");
+//     let a = "hrammm";
+//     console.log(a);
+// })();
+
+
+
+ //let a = "vipul";
+
+   
+  //let b = a.includes("v");
+  //console.log(b);
+
+  
+ 
+//    for(a=1; a<=10; a++){
+       
+//     console.log(a)
+    
+
+//    }
+//     let total = 0;
+//    let i = 0;
+//     while(i<=10){
+//         sum = total+i;
+//         i++;
+//     }
+//     console.log(sum);
+
+
+//     let rav = "vipul singh";
+//     let gau ="sonu singh";
+//      console.log(`hi tmy name is ${rav} ${gau}.`);
+
+//  let sum=0;
+// for(let r =1; r<=10; r++){
+//     sum+=r;
+// }
+// console.log(sum)
+
+// let yyy=0;
+// let xxx=1
+// while(xxx<=10){
+//     yyy+=xxx
+//     xxx++;
+// }
+// console.log(yyy);
+
+//  function sum(a ,b){
+//     document.write(a+b);
+
+//  }
+// sum(9,0);
+    
+
+
+function exampal(name="vipul"){
+    return `hiii iam  ${name}!`;
+
+}
+   const result = exampal();
+   console.log(result);
+
+
+
+
+     
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
